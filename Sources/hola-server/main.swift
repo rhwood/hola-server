3c0de6ca88580268750dc517ffd71344199c79f3
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-11-11 06:14:15 -0500

    chore: linting

--- Sources/hola-server/main.swift
+++ Sources/hola-server/main.swift
@@ -5,25 +5,25 @@ import Swifter
 import ArgumentParser
 
 class HolaServer {
-    
+
     var port: UInt16
     let bonjour: NetService!
     let server = HttpServer()
     let semaphore = DispatchSemaphore(value: 0)
-    
+
     init(_ port: UInt16) {
         self.port = port
         bonjour = NetService(domain: "local.", type: "_http._tcp.", name: "Local Service", port: Int32(port))
         let service = "/service"
         let files = "/files/:path"
+        let sources = "https://github.com/rhwood/hola-server"
         bonjour.setTXTRecord(NetService.data(fromTXTRecord: ["path": service.data(using: .utf8)!]))
         server[service] = scopes {
             html {
                 head {
                     meta { charset = "UTF-8" }
                     element("meta",
-                            ["viewport": "width=device-width, initial-scale=1.0, user-scalable=yes"],
-                            {}
+                            ["viewport": "width=device-width, initial-scale=1.0, user-scalable=yes"], {}
                     )
                 }
                 body {
@@ -35,14 +35,14 @@ class HolaServer {
                         inner = "Hola Browser discovers running HTTP services ."
                     }
                     p {
-                        inner = "Source code at <a href=\"https://github.com/rhwood/hola-server\">https://github.com/rhwood/hola-server</a>."
+                        inner = "Source code at <a href=\"\(sources)\">\(sources)</a>."
                     }
                 }
             }
         }
         server[files] = directoryBrowser("/")
     }
-    
+
     func start() {
         do {
             try server.start(port, forceIPv4: true)
@@ -54,7 +54,7 @@ class HolaServer {
             semaphore.signal()
         }
     }
-    
+
     func stop() {
         bonjour.stop()
         server.stop()
