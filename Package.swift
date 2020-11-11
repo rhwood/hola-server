d8371813e0d0da361517fa9b6d380fb1f66c99bf
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-11-11 05:49:36 -0500

    chore: Update dependencies

--- Package.swift
+++ Package.swift
@@ -13,19 +13,15 @@ let package = Package(
             targets: [projectName])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/httpswift/swifter.git", .upToNextMajor(from: "1.4.7")
-            .branch("stable")),
-        .package(url: "https://github.com/IBM-Swift/BlueSignals.git", .upToNextMinor(from: "1.0.21"))
+        .package(url: "https://github.com/httpswift/swifter.git", .upToNextMajor(from: "1.5.0")),
+        .package(url: "https://github.com/IBM-Swift/BlueSignals.git", .upToNextMajor(from: "1.0.200")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: projectName,
             dependencies: ["Swifter", "Signals"]),
         .testTarget(
             name: "hola-serverTests",
-            dependencies: [projectName]),
+            dependencies: [projectName])
     ]
 )
